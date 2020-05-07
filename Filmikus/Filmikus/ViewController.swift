17da8ab9769307d5a0b2841254742fa7e4a88a85
Andrew Kozlov <andrey.kozlov@ironwaterstudio.com>
2020-05-07 13:00:02 +0300

    create project

---
+++ Filmikus/Filmikus/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Filmikus
+//
+//  Created by Андрей Козлов on 07.05.2020.
+//  Copyright © 2020 Андрей Козлов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
