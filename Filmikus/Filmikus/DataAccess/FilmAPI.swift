923564dbfd1d71990a26dcfa8b07b57da030137f
Andrew Kozlov <andrey.kozlov@ironwaterstudio.com>
2020-05-25 17:11:31 +0300

    add pod Moya

---
+++ Filmikus/Filmikus/DataAccess/FilmAPI.swift
@@ -0,0 +1,58 @@
+//
+//  FilmAPI.swift
+//  Filmikus
+//
+//  Created by Андрей Козлов on 25.05.2020.
+//  Copyright © 2020 Андрей Козлов. All rights reserved.
+//
+
+import Moya
+
+enum FilmAPI {
+	static let apiKey = ""
+	case films
+}
+
+extension FilmAPI: TargetType {
+	
+	var baseURL: URL {
+		
+		URL(string: "")!
+	}
+	
+	var path: String {
+		switch self {
+		case .films:
+			return "/films"
+		}
+	}
+	
+	var method: Method {
+		switch self {
+		case .films:
+			return .get
+		}
+	}
+	
+	var sampleData: Data {
+		switch self {
+		case .films:
+			return "Test films".data(using: .utf8)!
+		}
+	}
+	
+	var task: Task {
+		switch self {
+		case .films:
+			let params = [
+                "APPID": Self.apiKey,
+                "id": ""
+            ]
+            return .requestParameters(parameters: params, encoding: URLEncoding.queryString)
+		}
+	}
+	
+	var headers: [String : String]? {
+		["Content-type": "application/json"]
+	}
+}
